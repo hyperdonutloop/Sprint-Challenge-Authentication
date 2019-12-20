788ad1c18ece860bb177c2f0dcd6feb1892435a9
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-20 13:11:05 -0800

    completed two test per endpoint and testing has no errors

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,9 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
+  const secret = process.env.JWT_SECRET || 'cookies are delicious';
   if (authorization) {
-    const secret = process.env.JWT_SECRET || 'cookies are delicious';
     jwt.verify(authorization, secret, function(error, decodedToken) {
       if(error) {
         res.status(401).json({ you: 'shall not pass! 💀' });
--- users/users-router.js
+++ users/users-router.js
@@ -1,14 +1,15 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/authenticate-middleware.js');
+// const Users = require('./users-model.js');
+// const restricted = require('../auth/authenticate-middleware.js');
 
-router.get('/', restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
-});
+// router.get('/', restricted, (req, res) => {
+//   Users.find()
+//     .then(users => {
+//       res.json(users);
+//     })
+//     .catch(err => res.send(err));
+// });
 
-module.exports = router;
+// module.exports = router;
+
