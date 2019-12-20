030c8534c215a7f40c5c1406d1675408918189ed
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-20 08:40:45 -0800

    register, login, and authenticate middleware are done. tested with insomnia and all endpoints are working

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,19 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || 'cookies are delicious';
+    jwt.verify(authorization, secret, function(error, decodedToken) {
+      if(error) {
+        res.status(401).json({ you: 'shall not pass! 💀' });
+      } else {
+        req.token = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ message: 'Please login and try again!' })
+  }
 };
